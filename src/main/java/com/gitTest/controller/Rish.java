62a29ff93b67d9218777422d38c85f2bf24aa3bf
rishpatil123 <rishpatil123@gmail.com>
2022-11-07 17:12:45 +0530

    Project Updated

---
+++ src/main/java/com/gitTest/controller/Rish.java
@@ -0,0 +1,10 @@
+package com.gitTest.controller;
+
+public class Rish {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("well to GitHub Project");
+	}
+
+}

08dabee47968320f915543bc859c36d803a1c295
rishpatil123 <rishpatil123@gmail.com>
2022-11-07 17:03:15 +0530

    Done

--- src/main/java/com/gitTest/pravin/Rish.java
+++ src/main/java/com/gitTest/pravin/Rish.java
@@ -4,7 +4,8 @@ public class Rish {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("well Come My Club");
+System.out.println("well Come To My Club");
+System.out.println(" My 1st Git Project");
 	}
 
 }
